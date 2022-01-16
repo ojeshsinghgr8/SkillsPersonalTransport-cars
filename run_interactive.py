7d4845ecaa78f060cb764e04d421ab0be974b261
ojeshsinghgr8 <ojeshsinghgr8@gmail.com>
2022-01-16 11:54:44 +0400

    Fixed error in run_interactively

--- run_interactive.py
+++ run_interactive.py
@@ -27,9 +27,9 @@ def turn_handler(
     out_response = ctx.last_response
     # the next condition branching needs for testing
     if true_out_response is not None and true_out_response != out_response:
-        raise Exception(f"{in_request=} -> true_out_response != out_response: {true_out_response} != {out_response}")
+        raise Exception(f"{in_request} -> true_out_response != out_response: {true_out_response} != {out_response}")
     else:
-        print(f"{in_request=} -> {out_response}")
+        print(f"{in_request} -> {out_response}")
     return out_response, ctx
 
 
